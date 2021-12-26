929df6b656c37d883b9b83fe3856aa3c3563204f
griends1 <Steelpan1>
2021-12-25 16:30:46 -0800

    Initial Commit

---
+++ Restart App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart App
+//
+//  Created by Sebastiaan van de Griendt on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
